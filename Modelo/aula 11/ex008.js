bb453d7524c82f9f644af26d8210df5bc5034561
Gabriel Bento <91234491+GBDMA@users.noreply.github.com>
2022-05-30 18:15:04 -0300

    Add files via upload

---
+++ Modelo/aula 11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 65
+console.log(`A velocidade do seu corpo é ${vel}Km/h`)
+if (vel > 60){ // Condição Simples!!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+
+
+console.log('Dirija sempre usando cinto de segurança')
